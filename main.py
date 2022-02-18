b565ec937451a4263d91d706679f94d271062e3e
Emeka <mjbanks4real@gmail.com>
2022-02-18 04:00:35 +0000

    removed some lines of code

--- main.py
+++ main.py
@@ -6,15 +6,6 @@ names = names_string.split(", ")
 #Write your code below this line 👇
 import random
 
-num_items = len(names)
-#Generate random numbers between 0 and the last index. 
-random_choice = random.randint(0, num_items - 1)
-#Pick out random person from list of names using the random number.
-person_who_will_pay = names[random_choice]
-
-print(person_who_will_pay + " is going to buy the meal today!")
-
-
 name1 = len(names)
 
 name_rand = random.randint(0, name1 - 1)
